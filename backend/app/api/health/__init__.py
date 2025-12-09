21bc414b7d5cba8caeed7c81e35e0b3dbb4197f0
pd241008 <142252602+pd241008@users.noreply.github.com>
2025-12-09 19:17:59 +0530

    Rename and Refactor

--- backend/app/api/health/__init__.py
+++
@@ -1,3 +0,0 @@
-# backend/app/api/health/__init__.py
-# Use a relative import to get the 'router' variable defined in health.py
-from .health import router
--- backend/app/api/upload/__init__.py
+++
@@ -1,3 +0,0 @@
-# backend/app/api/upload/__init__.py
-# Use a relative import to get the 'router' variable defined in upload.py
-from .upload import router
